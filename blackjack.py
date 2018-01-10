6f041a6d36cad391819ff38e6fc53a9bd9dbb2e6
loathsome-turtle <35227614+loathsome-turtle@users.noreply.github.com>
2018-01-10 09:15:35 -0500

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,15 @@
+import math 
+import random
 
+cardValues = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+suits = ['Spades','Hearts','Diamonds','Clubs']
+
+computerCards = []
+computerValue = 0
+playerCards = []
+playerValue = 0
+
+def deal():
+  computerCards = [cardValues[random.randint()], cardValues[random.randint()]]
+  
+  
