f3668ce27f1da950c4f12c316f3d164977750b9b
skipperhi <skipperhi42@gmail.com>
2018-01-11 13:10:52 -0500

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -40,7 +40,7 @@ playerDeal()
 playerValue = sum(playerCards)
 computerValue = sum(computerCards)
 
-# Introduction
+# Introduction, Includes Title, Names, Description and Directions
 print("Welcome to Blackjack! The goal of the game is to get as close to 21 as possible, without going over. Type 'h' or 'hit' to draw a card, and type 's' or 'stand' when you're done")
 print("Made by Tony and Jia Ming")
 printCards(playerCars,computerCards)
