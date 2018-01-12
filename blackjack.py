6d1c1965afba51c119d1d02463281c9e5774279b
loathsome-turtle <35227614+loathsome-turtle@users.noreply.github.com>
2018-01-12 08:40:44 -0500

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -4,10 +4,6 @@ import sys
 
 # Creating Variables
 suits = ['Spades','Hearts','Diamonds','Clubs']
-computerCards = []
-computerValue = 0
-playerCards = []
-playerValue = 0
 
 # A Function for Getting a Card
 def getCards():
@@ -40,65 +36,87 @@ def printAllCards(player, computer):
   print('%s' % ', '.join(map(str, player)))
   print("Computer's Cards: ", end = '')
   print('%s' % ', '.join(map(str, computer)))
-
-def sumOfCards(cards, value):
-  for card in cards:
-    if card == 1 and value < 21:
-      value += 11
-    else:
-      value += card
-
-# Dealing and setting the Value
-computerDeal()
-playerDeal()
-playerValue = sum(playerCards)
-computerValue = sum(computerCards)
-
-# Introduction, Includes Title, Names, Description and Directions
-print("Welcome to Blackjack! The goal of the game is to get as close to 21 as possible, without going over. Type 'h' or 'hit' to draw a card, and type 's' or 'stand' when you're done")
-print("Made by Tony and Jia Ming")
-printPlayerCards(playerCards, computerCards)
-
-# Allows the Player to Draw Cards
-while True:
   
-  # Exits if the Player Has Busted
-  if playerValue > 21:
-    print("You've gone over 21! You lost!")
+# A Function to ask if the player would like to play again
+def playAgain():
+  answer = input("Would you like to play again? Yes/No")
+  answer = answer.lower()
+  if answer == 'yes' or answer == 'y':
+    game()
+  if answer == 'no' or answer == 'n':
+    print('Thank you for playing. Please come again!")
     sys.exit()
-  
-  # Asking the Player to Hit or Stand
-  action = input("h/it or s/tand?")
-  action = action.lower()
-  
-  # Carrying out the Commands of the Player
-  if action == "hit" or action == "h":
-    playerCards.append(getCards())
+  else:
+    print('I\'m not quite sure what you said')
+    playAgain()
+                 
+# MAIN GAME FUNCTION
+def game():
+  #setting variables
+  computerCards = []
+  computerValue = 0
+  playerCards = []
+  playerValue = 0
+
+  # Dealing and setting the Value
+  computerDeal()
+  playerDeal()
+  playerValue = sum(playerCards)
+  computerValue = sum(computerCards)
+  printPlayerCards(playerCards, computerCards)
+
+  # Allows the Player to Draw Cards
+  while True:
+    
+    # Exits if the Player Has Busted
     if playerValue > 21:
-      printAllCards(playerCards, computerCards)
       print("You've gone over 21! You lost!")
       sys.exit()
-    playerValue = sum(playerCards)
-    printPlayerCards(playerCards, computerCards)
-  elif action == "stand" or action == "s":
-    break
-  else:
-    print("I'm not sure what you said, try again!")
+    
+    # Asking the Player to Hit or Stand
+    action = input("Hit or Stand?")
+    action = action.lower()
+    
+    # Carrying out the Commands of the Player
+    if action == "hit" or action == "h":
+      playerCards.append(getCards())
+      if playerValue > 21:
+        printAllCards(playerCards, computerCards)
+        print("You've gone over 21! You lost!")
+        playAgain()
+      playerValue = sum(playerCards)
+      printPlayerCards(playerCards, computerCards)
+    elif action == "stand" or action == "s":
+      break
+    else:
+      print("I'm not sure what you said, try again!")
 
-# Allowing the Computer to Draw Cards Until It's Busted or It's Score is Higher Than the Player's
-while (computerValue < playerValue):
-  computerCards.append(getCards())
-  computerValue = sum(computerCards)
-  if computerValue > 21 and playerValue <= 21:
-    printAllCards(playerCards, computerCards)
-    print("You Win! The computer has busted.")
-    sys.exit()
+  # Allowing the Computer to Draw Cards Until It's Busted or It's Score is Higher Than the Player's
+  while (computerValue < playerValue):
+    computerCards.append(getCards())
+    computerValue = sum(computerCards)
+    if computerValue > 21 and playerValue <= 21:
+      printAllCards(playerCards, computerCards)
+      print("You Win! The computer has busted.")
+      playAgain()
 
-# Evaluating The Outcome of the Game
-printAllCards(playerCards, computerCards)
-if playerValue > computerValue and playerValue <= 21:
-  print("You Win!")
-elif playerValue == computerValue and playerValue <= 21:
-  print("It's a Tie!")
-elif playerValue < computerValue and playerValue <= 21 and computerValue <= 21:
-  print("The computer got a higher score, you lost!")
+  # Evaluating The Outcome of the Game
+  printAllCards(playerCards, computerCards)
+  if playerValue > computerValue and playerValue <= 21:
+    print("You Win!")
+    playAgain()
+  elif playerValue == computerValue and playerValue <= 21:
+    print("It's a Tie!")
+    playAgain()
+  elif playerValue < computerValue and playerValue <= 21 and computerValue <= 21:
+    print("The computer got a higher score, you lost!")
+    playAgain()
+
+# Introduction, Includes Title, Names, Description and Directions
+try:
+  print("Welcome to Blackjack! The goal of the game is to get as close to 21 as possible, without going over. Type 'h' or 'hit' to draw a card, and type 's' or 'stand' when you're done")
+  print("Made by Tony and Jia Ming")
+  game()
+except:
+  print('Uh Oh, looks like something isnt\'t working right now. Please try restarting the game.")
+  sys.exit()
