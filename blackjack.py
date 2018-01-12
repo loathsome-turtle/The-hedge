2d9af55690c48b798aa386f733b5af8219edfadb
loathsome-turtle <35227614+loathsome-turtle@users.noreply.github.com>
2018-01-12 08:59:20 -0500

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -2,66 +2,60 @@
 import random
 import sys
 
-# Creating Variables
-suits = ['Spades','Hearts','Diamonds','Clubs']
-computerCards = []
-computerValue = 0
-playerCards = []
-playerValue = 0
-
-# A Function for Getting a Card
-def getCards():
-  card = random.randint(1,13)
-  if card > 10:
-    card = 10
-  return card
-
-# A Function to Deal Cards to the Computer
-def computerDeal():
-  computerCards.append(getCards())
-  computerCards.append(getCards())
-
-# A Function to Deal Cards to the Player
-def playerDeal():
-  playerCards.append(getCards())
-  playerCards.append(getCards())
-  
-# A Function to Print the Player's Cards and the Computer's First Card
-def printPlayerCards(player, computer):
-  print("Your Cards: ", end = '')
-  print('%s' % ', '.join(map(str, player)))
-  print("Computer Cards: ", end = '')
-  print(computer[0], end = '')
-  print(", X")
-
-# A Function to Print the Player's Cards and the Computer's Cards
-def printAllCards(player, computer):
-  print("Your Cards: ", end = '')
-  print('%s' % ', '.join(map(str, player)))
-  print("Computer's Cards: ", end = '')
-  print('%s' % ', '.join(map(str, computer)))
-  
 # A Function to ask if the player would like to play again
 def playAgain():
-  answer = input("Would you like to play again? Yes/No")
+  answer = input("Would you like to play again? Yes/No ")
   answer = answer.lower()
   if answer == 'yes' or answer == 'y':
     game()
   if answer == 'no' or answer == 'n':
-    print("Thank you for playing. Please come again!")
+    print('Thank you for playing. Please come again!')
     sys.exit()
   else:
-    print("I'm not quite sure what you said")
+    print('I\'m not quite sure what you said')
     playAgain()
                  
 # MAIN GAME FUNCTION
 def game():
-  #setting variables
+  # Creating Variables
+  suits = ['Spades','Hearts','Diamonds','Clubs']
   computerCards = []
   computerValue = 0
   playerCards = []
   playerValue = 0
 
+  # A Function for Getting a Card
+  def getCards():
+    card = random.randint(1,13)
+    if card > 10:
+      card = 10
+    return card
+
+  # A Function to Deal Cards to the Computer
+  def computerDeal():
+    computerCards.append(getCards())
+    computerCards.append(getCards())
+
+  # A Function to Deal Cards to the Player
+  def playerDeal():
+    playerCards.append(getCards())
+    playerCards.append(getCards())
+    
+  # A Function to Print the Player's Cards and the Computer's First Card
+  def printPlayerCards(player, computer):
+    print("Your Cards: ", end = '')
+    print('%s' % ', '.join(map(str, player)))
+    print("Computer Cards: ", end = '')
+    print(computer[0], end = '')
+    print(", X")
+
+  # A Function to Print the Player's Cards and the Computer's Cards
+  def printAllCards(player, computer):
+    print("Your Cards: ", end = '')
+    print('%s' % ', '.join(map(str, player)))
+    print("Computer's Cards: ", end = '')
+    print('%s' % ', '.join(map(str, computer)))
+     
   # Dealing and setting the Value
   computerDeal()
   playerDeal()
@@ -75,10 +69,10 @@ def game():
     # Exits if the Player Has Busted
     if playerValue > 21:
       print("You've gone over 21! You lost!")
-      sys.exit()
+      playAgain()
     
     # Asking the Player to Hit or Stand
-    action = input("Hit or Stand?")
+    action = input("Hit or Stand? ")
     action = action.lower()
     
     # Carrying out the Commands of the Player
@@ -117,10 +111,8 @@ def game():
     playAgain()
 
 # Introduction, Includes Title, Names, Description and Directions
-try:
-  print("Welcome to Blackjack! The goal of the game is to get as close to 21 as possible, without going over. Type 'h' or 'hit' to draw a card, and type 's' or 'stand' when you're done")
-  print("Made by Tony and Jia Ming")
-  game()
-except:
-  print("Uh Oh, looks like something isn't working right now. Please try restarting the game.")
-  sys.exit()
+
+print("Welcome to Blackjack! The goal of the game is to get as close to 21 as possible, without going over. Type 'h' or 'hit' to draw a card, and type 's' or 'stand' when you're done")
+print("Made by Tony and Jia Ming")
+game()
+
